4070bab43319a3e377db248639b80b223200befb
James Munns <james.munns@gmail.com>
2016-12-28 19:04:58 +0100

    Update to match python API a little better, improve docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,24 @@
 //! Generate human readable digests for UUIDs
 //!
 //! Based on https://github.com/zacharyvoase/humanhash
+//! Should be compatible
 
 extern crate uuid;
-
 use uuid::Uuid;
 
-const DEFAULT_WORDLIST: &'static [ &'static str ] = &[
+/// Class for custom humanhashers
+pub struct HumanHasher {
+    words: Wordlist,
+}
+
+/// List of 256 strings usable for human readable hash digests
+pub type Wordlist = &'static [&'static str; 256];
+
+/// Instance of human hasher with default wordlist
+const DEFAULT_HUMANIZER: HumanHasher = HumanHasher { words: DEFAULT_WORDLIST };
+
+/// Default Wordlist, chosen to match the original Python Human Hash
+pub const DEFAULT_WORDLIST: Wordlist = &[
     "ack", "alabama", "alanine", "alaska", "alpha", "angel", "apart", "april",
     "arizona", "arkansas", "artist", "asparagus", "aspen", "august", "autumn",
     "avocado", "bacon", "bakerloo", "batman", "beer", "berlin", "beryllium",
@@ -44,6 +56,28 @@ const DEFAULT_WORDLIST: &'static [ &'static str ] = &[
     "wisconsin", "wolfram", "wyoming", "xray", "yankee", "yellow", "zebra",
     "zulu" ];
 
+/// Human Hasher
+impl HumanHasher {
+    /// Create a new hasher with a custom wordlist
+    pub fn new(words: Wordlist) -> HumanHasher {
+        HumanHasher { words: words }
+    }
+
+    /// Create a human readable digest for a UUID. Makes the collision space worse,
+    /// reducing it to 1:(2^(8*`words_out`)-1).
+    pub fn humanize(&self, uuid: &Uuid, words_out: usize) -> String {
+        compress(uuid.as_bytes(), words_out)
+            .iter()
+            .map(|&x| self.words[x as usize].to_string())
+            .collect::<Vec<String>>()
+            .join("-")
+    }
+}
+
+/// Break a slice of u8s into (at least) `target` `u8`s.
+///
+/// WARNING: If the slice is not evenly divisible, there will be one extra u8
+/// from the remainder. output `u8`s are created by XORing the input bytes.
 fn compress(bytes: &[u8], target: usize) -> Vec<u8> {
     let seg_size = bytes.len() / target;
     bytes.chunks(seg_size)
@@ -51,19 +85,17 @@ fn compress(bytes: &[u8], target: usize) -> Vec<u8> {
         .collect::<Vec<u8>>()
 }
 
-pub fn digest_four(uuid: &Uuid) -> String {
-    compress(uuid.as_bytes(), 4)
-        .iter()
-        .map(|&x| DEFAULT_WORDLIST[x as usize].to_string())
-        .collect::<Vec<String>>()
-        .join("-")
+/// Create a human readable digest for a UUID. Makes the collision space worse,
+/// reducing it to 1:(2^(8*`words_out`)-1).
+pub fn humanize(uuid: &Uuid, words_out: usize) -> String {
+    DEFAULT_HUMANIZER.humanize(uuid, words_out)
 }
 
 #[cfg(test)]
 mod tests {
     use super::uuid::Uuid;
     use super::DEFAULT_WORDLIST;
-    use super::digest_four;
+    use super::{digest_four, humanize, HumanHasher};
 
     const TEST_UUID: &'static str = "bc0f47f93dd046578d7eee645999b95e";
 
@@ -71,8 +103,22 @@ mod tests {
     fn it_works() {
         let tuid = Uuid::parse_str(TEST_UUID).unwrap();
 
-        let x = digest_four(&tuid);
+        assert_eq!(humanize(&tuid, 4), "august-yankee-lima-coffee");
 
-        assert_eq!(x, "august-yankee-lima-coffee");
+        assert_eq!("pip", humanize(&tuid, 1));
+        assert_eq!("washington-hot", humanize(&tuid, 2));
+        assert_eq!("august-yankee-lima-coffee", humanize(&tuid, 4));
+        assert_eq!("princess-sad-victor-bakerloo-whiskey-mike-saturn-uniform",
+                   humanize(&tuid, 8));
+
+    }
+
+    #[test]
+    fn class_works() {
+        let tuid = Uuid::parse_str(TEST_UUID).unwrap();
+
+        let hzr = HumanHasher::new(&DEFAULT_WORDLIST);
+
+        assert_eq!(humanize(&tuid, 4), hzr.humanize(&tuid, 4));
     }
 }
